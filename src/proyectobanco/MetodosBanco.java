8cc715ac4e0e070e3a2af1f9e8b25f0f02539808
Pablo Uraga Martinez <emptywhitelist@gmail.com>
2023-02-15 19:37:34 +0100

    Banco

--- src/proyectobanco/MetodosBanco.java
+++ src/proyectobanco/MetodosBanco.java
@@ -12,41 +12,45 @@ import java.util.Scanner;
  * @author pabmar
  */
 public class MetodosBanco {
-    ArrayList <Cuenta> cuentas= new ArrayList();
+
+    ArrayList<Cuenta> cuentas = new ArrayList();
     private int total = 0;
-    public MetodosBanco(){
+
+    public MetodosBanco() {
         cuentas = new ArrayList();
-        
+
     }
-    
+
     Scanner teclado = new Scanner(System.in);
 //    En una primera versión para almacenar en memoria los clientes
 //    construiremos un array de Cuenta
-    
+
 //    (En una segunda versión para almacenar en memoria los clientes
 //    construiremos una lista de Cuenta)
-    
-    
-    
-    public Cuenta buscarCuenta(){
+    public Cuenta buscarCuenta() {
         System.out.println("1. Buscar por nombre\n2. Buscar por numero");
         int eleccion = teclado.nextInt();
-        
-        switch(eleccion){
+
+        switch (eleccion) {
             case 1:
+                teclado.nextLine();
                 System.out.println("Introduce el nombre de la cuenta: ");
                 String nombreCuenta = teclado.nextLine();
                 poscuentanombre(nombreCuenta);
-                
+                break;
             case 2:
+                teclado.nextLine();
                 System.out.println("Introduce el numero de cuenta");
                 String numcta = teclado.nextLine();
                 poscuenta(numcta);
+                break;
+            default:
+                break;
         }
         return null;
     }
-    
-    public Cuenta poscuentanombre(String nombre){
+
+    public Cuenta poscuentanombre(String nombre) {
         for (int i = 0; i < cuentas.size(); i++) {
             if (cuentas.get(i).getNombre().equals(nombre)) {
                 return cuentas.get(i);
@@ -54,8 +58,8 @@ public class MetodosBanco {
         }
         return null;
     }
-    
-    public Cuenta poscuenta(String numcta){
+
+    public Cuenta poscuenta(String numcta) {
         for (int i = 0; i < cuentas.size(); i++) {
             if (cuentas.get(i).getCuenta().equals(numcta)) {
                 return cuentas.get(i);
@@ -63,31 +67,37 @@ public class MetodosBanco {
         }
         return null;
     }
-    
-    
-    
-    public void NuevaCuenta(){
+
+    public void NuevaCuenta() {
         boolean repetida = false, control = true;
         Cuenta nueva = new Cuenta();
-        
+
         do {
-            try{
+            try {
+                String nombreCuenta = "";
                 control = true;
-                System.out.println("Introduce el nombre: ");
-                nueva.setNombre(teclado.nextLine());
-            } catch(Exception e){
+                do {
+                    System.out.println("Introduce el nombre de la cuenta");
+                    nombreCuenta = teclado.nextLine();
+                    nueva.setNombre(nombreCuenta);
+                } while (nombreCuenta == null);
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
                 control = false;
             }
         } while (!control);
-        
+
         do {
             do {
-                try{
+                try {
+                    String numeroCuenta = "";
                     control = true;
-                    System.out.println("Introduce el numero de cuenta");
-                    nueva.setCuenta(teclado.nextLine());
-                } catch(Exception e){
+                    do {
+                        System.out.println("Introduce el numero de cuenta");
+                        numeroCuenta = teclado.nextLine();
+                        nueva.setCuenta(numeroCuenta);
+                    } while (numeroCuenta == null);
+                } catch (Exception e) {
                     System.out.println(e.getMessage());
                     control = false;
                 }
@@ -96,40 +106,106 @@ public class MetodosBanco {
                 if (cuentas.get(i).getCuenta().equals(nueva.getCuenta())) {
                     System.out.println("Cuenta repetida");
                     repetida = true;
-                    i=cuentas.size();
-                } else{
+                    i = cuentas.size();
+                } else {
                     repetida = false;
                 }
             }
         } while (repetida);
         do {
-            try{
+            try {
+                double saldo = 0;
                 control = true;
-                System.out.println("Introduce el saldo");
-                nueva.setSaldo(teclado.nextDouble());
-            } catch (Exception e){
+                do {
+                    System.out.println("Introduce el saldo");
+                    saldo = teclado.nextDouble();
+                    nueva.setSaldo(saldo);
+                } while (saldo == 0);
+
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
                 control = false;
             }
         } while (!control);
-        
-        do{
-            try{
+
+        do {
+            try {
+                double interes = 0;
                 control = true;
-                System.out.println("Introduce el tipo de interes");
-                nueva.setTipoDeInteres(teclado.nextDouble());
-            } catch(Exception e){
+                do {
+                    System.out.println("Introduce el tipo de interes");
+                    interes = teclado.nextDouble();
+                    nueva.setTipoDeInteres(interes);
+                } while (interes == 0);
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
                 control = false;
             }
-        } while(!control);
-        
+        } while (!control);
+
         cuentas.add(nueva);
-        
+
         System.out.println("Cuenta añadida");
-        
+
         total = total + 1;
     }
-    
-    
+
+    public void ModificarCuenta() {
+
+        int posicion = cuentas.indexOf(buscarCuenta());
+
+//        Cambiar nombre
+        System.out.println("Introduce el nuevo Nombre (0 si quiere mantener el nombre actual) ");
+        try {
+            String nuevoNombre = teclado.nextLine();
+            if (nuevoNombre == null) {
+                cuentas.get(posicion).setNombre(cuentas.get(posicion).getNombre());
+            } else {
+                cuentas.get(posicion).setNombre(nuevoNombre);
+            }
+        } catch (Exception a) {
+            System.out.println(a.getMessage());
+        }
+
+//        Cambiar numero
+        System.out.println("Introduce el nuevo Numero de cuenta: ");
+        try {
+            String nuevoNumero = teclado.nextLine();
+            if (nuevoNumero == null) {
+                cuentas.get(posicion).setCuenta(cuentas.get(posicion).getCuenta());
+            } else {
+                cuentas.get(posicion).setCuenta(nuevoNumero);
+            }
+        } catch (Exception a) {
+            System.out.println(a.getMessage());
+        }
+
+//        Cambiar Saldo
+        System.out.println("Introduce el nuevo Saldo: ");
+        try {
+            Double nuevoSaldo = teclado.nextDouble();
+            if (nuevoSaldo == null) {
+                cuentas.get(posicion).setSaldo(cuentas.get(posicion).getSaldo());
+            } else {
+                cuentas.get(posicion).setSaldo(nuevoSaldo);
+            }
+        } catch (Exception a) {
+            System.out.println(a.getMessage());
+        }
+
+//        Cambiar Interes
+        System.out.println("Introduce el nuevo Interes: ");
+        try {
+            Double nuevoInteres = teclado.nextDouble();
+            if (nuevoInteres == null) {
+                cuentas.get(posicion).setTipoDeInteres(cuentas.get(posicion).getTipoDeInteres());
+            } else {
+                cuentas.get(posicion).setTipoDeInteres(nuevoInteres);
+            }
+        } catch (Exception a) {
+            System.out.println(a.getMessage());
+        }
+
+    }
+
 }
